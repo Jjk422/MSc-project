b13aff484c6514704a51f510ac279f9c091da2d0
Jason Keighley <jjkeighley@gmail.com>
2019-07-15 10:40:11 +0100

    Major changes: - Added iteration numbering on scripts - Added question generator which uses definitions to generate questions
    Minor changes:
    - Added list of ITIL acronyms
    - Added list of ITIL concept definitions
    - Started adding the part of speech regex class
    
    Bugs/Fixes/TODOs:
    - Need to link all scripts together into one continuous program
    - Need to improve functionality
    - Need to calculate metrics for all script tasks
    - Need to implement question answer system and fact/material generator
    - Need to expand on question creator and automated ontology creator
    - Need to add better GUI/CLI for better user experience

---
+++ part_of_speech_regex/__init__.py
@@ -0,0 +1,13 @@
+class PartOfSpeechRegex:
+    @staticmethod
+    def test():
+        print("Test method")
+
+    # @staticmethod
+    # def parseAndReturnPatterns():
+    #     print("Test")
+
+    @staticmethod
+    def parseAndReturnPatterns(pattern_string, sentence):
+        # print(pattern_string)
+        print(sentence)
